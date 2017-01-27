f4d61a7f97d46ad27b53da805bf9f4faff4bdceb
Frank Fuhrmann <frank.fuhrmann@mailbox.org>
2017-01-27 20:35:24 +0100

    added support for $EXTRA_OPTS env var

--- run_es.sh
+++ run_es.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-su elasticsearch -c "/usr/share/elasticsearch/bin/elasticsearch -p /var/run/elasticsearch/elasticsearch.pid -Ddefault.path.logs=/var/log/elasticsearch -Ddefault.path.data=/var/lib/elasticsearch -Ddefault.path.conf=/etc/elasticsearch $1"
+su elasticsearch -c "/usr/share/elasticsearch/bin/elasticsearch -p /var/run/elasticsearch/elasticsearch.pid -Ddefault.path.logs=/var/log/elasticsearch -Ddefault.path.data=/var/lib/elasticsearch -Ddefault.path.conf=/etc/elasticsearch $EXTRA_OPTS $1"
