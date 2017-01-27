8efb471eb3f7dd949f8ee0a25cfdacd179dac69f
Frank Fuhrmann <frank.fuhrmann@mailbox.org>
2017-01-27 23:34:51 +0100

    added explicit privilege change of data directories

--- run_es.sh
+++ run_es.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# make sure we have the correct privileges
+chown elasticsearch:elasticsearch /data/elasticsearch/data
+chown elasticsearch:elasticsearch /data/elasticsearch/logs
+
+# run elasticsearch
 su elasticsearch -c "/usr/share/elasticsearch/bin/elasticsearch -p /var/run/elasticsearch/elasticsearch.pid -Ddefault.path.logs=/var/log/elasticsearch -Ddefault.path.data=/var/lib/elasticsearch -Ddefault.path.conf=/etc/elasticsearch $1 $EXTRA_OPTS"
